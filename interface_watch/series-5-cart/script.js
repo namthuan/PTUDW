c14145f4a2c41a44cfcaf15f96b5735487e61a80
ntnhan181 <anitahalley181@gmail.com>
2020-08-14 15:20:42 +0700

    update

--- interface_watch/series-5-cart/script.js
+++ interface_watch/series-5-cart/script.js
@@ -26,3 +26,7 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+function changeBorderColor() {
+  document.getElementById("my-table").style.border = "think solid blue";
+}
